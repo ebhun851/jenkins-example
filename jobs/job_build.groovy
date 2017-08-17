0bfc15596644781cd16e8218bcaef5c5a58ee670
Zakeeruddin <zakeeruddin_mohammed@gap.com>
2017-08-17 14:19:41 -0700

    nisum-000 [zakeer] Adding build job

---
+++ jobs/job_build.groovy
@@ -0,0 +1,42 @@
+def REMOTE_URL = 'https://github.com/rguruju/jenkins-demo.git'
+def BRANCH = 'master'
+
+job('build') {
+    logRotator(-1, 10)
+    scm {
+        git {
+            remote {
+                url("${REMOTE_URL}")
+            }
+            branch("${BRANCH}")
+            extensions {
+                cleanBeforeCheckout()
+            }
+        }
+    }
+    triggers {
+        githubPush()
+    }
+    wrappers {
+        buildUserVars()
+    }
+    steps {
+        gradle {
+            tasks('clean build uploadArchives')
+            switches('-P username=${BUILD_USER_ID}')
+            switches('--stacktrace -i')
+            useWrapper(true)
+        }
+    }
+    publishers {
+        publishHtml {
+            report('build/reports/tests/test') {
+                reportName('UnitTestReport')
+                reportFiles('index.html')
+                keepAll()
+                allowMissing()
+                alwaysLinkToLastBuild()
+            }
+        }
+    }
+}
